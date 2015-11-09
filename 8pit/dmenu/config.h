196e8e04e2315c09080357032ab417845c6b0b6c
Sören Tempel <soeren+git@soeren-tempel.net>
2015-11-09 13:36:30 +0100

    8pit/dmenu: increase font size

--- 8pit/dmenu/config.h
+++ 8pit/dmenu/config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"terminus:size=10"
+	"terminus:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the elft of input field */
 static const char *normbgcolor = "#282828"; /* -nb option; normal background                 */
