465b82aaba132d1ed685cbe5e8a3c15f25d5ee66
Sören Tempel <soeren+git@soeren-tempel.net>
2016-08-17 00:10:23 +0200

    8pit/slock: new aport

---
+++ 8pit/slock/config.h
@@ -0,0 +1,6 @@
+static const char *colorname[NUMCOLS] = {
+	"#282828",   /* after initialization */
+	"#585858",   /* during input */
+	"#282828",   /* failed/cleared the input */
+};
+static const Bool failonclear = True;
