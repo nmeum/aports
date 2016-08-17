05281ee08bca46352ddcbde99af773a0de5aca43
Sören Tempel <soeren+git@soeren-tempel.net>
2016-08-17 12:06:22 +0200

    8pit/slock: update colors

--- 8pit/slock/config.h
+++ 8pit/slock/config.h
@@ -1,6 +1,6 @@
 static const char *colorname[NUMCOLS] = {
 	"#282828",   /* after initialization */
 	"#585858",   /* during input */
-	"#282828",   /* failed/cleared the input */
+	"#AB4642",   /* failed/cleared the input */
 };
-static const Bool failonclear = True;
+static const Bool failonclear = False;
