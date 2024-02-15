dcf4951985dd0a43123b9f38c8aa384fa3769a9e
Sören Tempel <soeren+git@soeren-tempel.net>
2024-02-15 17:32:36 +0100

    8pit/dwm-status: only check for battery file once

--- 8pit/dwm-status/config.h
+++ 8pit/dwm-status/config.h
@@ -28,9 +28,17 @@ static const char* volumname = "Master Playback Volume";
 /* Only use batcap function if the device has a battery. */
 static size_t batcapmay(char *dest, size_t n) {
 	size_t ret;
+	static int hasbat = -1;
 
-	if (access(sysbat, F_OK))
+	if (hasbat == -1) {
+		if (access(sysbat, F_OK)) {
+			hasbat = 0;
+			return 0;
+		}
+		hasbat = 1;
+	} else if (!hasbat) {
 		return 0;
+	}
 
 	ret = batcap(dest, n);
 	if (ret)
